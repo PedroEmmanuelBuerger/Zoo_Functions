4da5bc60af3abe3e3dfda2b207a69c2e17722990
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-10-31 18:51:04 -0300

    resolvendo requesito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const nome = data.species.map((Element) => Element.name);
+    const quantidade = data.species.map((Element) => Element.residents.length);
+    const obj = {};
+    for (let i = 0; i < nome.length; i += 1) {
+      obj[nome[i]] = quantidade[i];
+    }
+    return obj;
+  }
+  const result = data.species.find((Element) => Element.name === animal.specie);
+  if (animal.sex === undefined) return result.residents.length;
+  return result.residents.filter((Element) => Element.sex === animal.sex).length;
 }
-
 module.exports = countAnimals;
