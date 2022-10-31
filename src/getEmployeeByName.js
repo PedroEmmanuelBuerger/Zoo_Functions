44fd76eed41e47f328f3c0e2a6846926591efdf9
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-10-31 16:03:12 -0300

    resolvendo requesito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  const nome = employeeName;
+  const result = data.employees.find((Element) => Element.firstName === nome
+  || Element.lastName === nome);
+  return result;
 }
 
 module.exports = getEmployeeByName;
