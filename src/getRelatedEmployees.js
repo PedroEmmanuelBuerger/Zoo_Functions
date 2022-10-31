7367f48bf9f0f3d0cfec841d222ac4f3ce7f2373
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-10-31 16:53:55 -0300

    resolvendo requesito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const pessoa = data.employees.find((Element) => Element.id === id);
+  const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+  const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+  const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+  const gerentes = [stephanieId, olaId, burlId];
+  return gerentes.some((Element) => Element === pessoa.id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
-}
+  const result = isManager(managerId);
+  if (result === true) {
+    const p = data.employees.filter((Eleme) => Eleme.managers.find((par2) => par2 === managerId));
+    return p.map((Element) => `${Element.firstName} ${Element.lastName}`);
+  }
 
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+}
 module.exports = { isManager, getRelatedEmployees };
