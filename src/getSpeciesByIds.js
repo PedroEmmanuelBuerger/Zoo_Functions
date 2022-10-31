939236a1e89f6a84f4d9bbe7902aa04d215677d5
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-10-31 15:15:39 -0300

    resolvendo requesito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length <= 0) return [];
+  if (ids.length === 1) {
+    const result = data.species.find((Element) => Element.id === ids[0]);
+    return [result];
+  }
+  const result = ids.map((Element) => data.species.find((Elements) => Elements.id === Element));
+  return result;
 }
 
 module.exports = getSpeciesByIds;
