b23a63f99640bdf0201aa1f2d40acdf7a617a958
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-11-01 16:09:21 -0300

    resolvendo requesito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const pessoa = data.employees.find((element) => element.id === id);
+  const animalId = pessoa.responsibleFor[0];
+  const animal = data.species.find((element) => element.id === animalId);
+  const older = animal.residents.reduce((a, b) => {
+    if (a.age > b.age) { return a; }
+    return b;
+  });
+  const { name, sex, age } = older;
+  return [name, sex, age];
 }
-
 module.exports = getOldestFromFirstSpecies;
