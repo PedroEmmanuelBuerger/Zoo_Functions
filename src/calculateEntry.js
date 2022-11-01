4dfe2748c36d5044864ed0b8bf2e607c2696fd11
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2022-11-01 15:19:13 -0300

    resolvendo requesito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const result = entrants.map((Element) => Element.age);
+  let child = 0;
+  let senior = 0;
+  let adult = 0;
+  result.forEach((Element) => {
+    if (Element < 18) { child += 1; }
+    if (Element >= 18 && Element < 50) { adult += 1; }
+    if (Element >= 50) { senior += 1; }
+  });
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-
+  if (!entrants || Object.entries(entrants).length === 0) return 0;
+  const arraypessoa = countEntrants(entrants);
+  const kids = arraypessoa.child * data.prices.child;
+  const adult = arraypessoa.adult * data.prices.adult;
+  const senior = arraypessoa.senior * data.prices.senior;
+  return senior + kids + adult;
 }
 
 module.exports = { calculateEntry, countEntrants };
